a7634b9a0e86db6694d7e1cea0896daca5d454f9
shokoh-inleventech <shokoh@inleventech.com>
2024-04-19 11:38:03 -0400

    configured some settings

--- build-docc.sh
+++ build-docc.sh
@@ -12,5 +12,5 @@ xcrun docc process-archive transform-for-static-hosting \
     --output-path ".docs" \
     --hosting-base-path "docc_publish_test" # add your repo name later
 
-# This will change the path host.com/documentation/your-product-name to host.com/your-product-name 
+# This will change the path.
 echo '<script>window.location.href += "/documentation/givenwithlove"</script>' > .docs/index.html
